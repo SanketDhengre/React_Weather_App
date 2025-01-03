6ae4bd9c9a433407a893db6025c4894ac0315183
Sanket Dhengre <sanketdhengre@gmail.com>
2025-01-04 02:37:16 +0530

    Completed the initial setup for the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { FaReact } from "react-icons/fa";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-7xl font-bold">Helloaa</h1>
+      <FaReact size={50} className="text-red-500"/>
+    
+    </div>
+  );
+};
+
+export default App;

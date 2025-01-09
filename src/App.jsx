fadec0b884c9d56eba73bf7aa4c1d59bf4440cc6
Sanket Dhengre <sanketdhengre@gmail.com>
2025-01-10 03:12:12 +0530

    completed the functionalities for the weatherSearvice

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 
+import Forecast from "./components/Forecast";
 import Inputs from "./components/Inputs";
+import TempAndDetails from "./components/TempAndDetails";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TopButtons from "./components/TopButtons";
+import getFormattedWeatherData from "./services/weatherService";
 
 const App = () => {
+
+  const getWeather = async () => {
+    const data = await getFormattedWeatherData({ q: "Nagpur" });
+    console.log(data);
+  }
+
+  getWeather();
+
   return (
     <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-xl shadow-gray-400 from-cyan-600 to-blue-700">
       <TopButtons />
       <Inputs />
 
       <TimeAndLocation />
+      <TempAndDetails />
+      <Forecast />
+      <Forecast />
     </div>
   );
 };
